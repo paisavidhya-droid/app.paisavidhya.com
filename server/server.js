75ceb4a23af7ae0957ba97d2eb28a1ee8b18e2d8
zeeshancodebase <sz.syedzeeshan1@gmail.com>
2026-03-10 21:39:43 +0530

    cors test

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ const PORT = process.env.PORT || 5000;
 app.set("trust proxy", 1);
 
 const corsOptions = {
-  // origin: true, // reflects the request origin
+  origin: true, // reflects the request origin
   // origin: process.env.FRONTEND_URL || "http://localhost:3000",
   // origin: '*', 
   credentials: true, // 🔥 required for cookies, auth headers
